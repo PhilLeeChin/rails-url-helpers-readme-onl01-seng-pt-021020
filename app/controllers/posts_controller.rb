67645b1355693d3d614074a8220b5de75715ed3d
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-17 19:17:30 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,12 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+	describe 'index page' do
+		it 'links to post page' do
+			second_post = Post.create(title: "My Title", description: "My post description")
+			visit posts_path
+			expect(page).to have_link(second_post.title, href: post_path(second_post))
+		end
+	end
 end
